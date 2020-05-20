1f39d362d734b792e1a10690d7b89b9f059b598c
Amulya Mohan <ammohan@microsoft.com>
2020-05-20 11:46:58 -0700

    login

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,14 @@
+import warnings
 import mwclient
 import requests
 from credentials import usr, pas
 
 site = mwclient.Site("myfirsttestwiki.fandom.com", path="/")
 
-if !(usr and (not usr.isspace())) and !(pas and (not pas.isspace())):
-site.force_login = False
+if not (usr and (not usr.isspace())) or not (pas and (not pas.isspace())):
+    warnings.warn("Put your fandom username and password in credentials.py (don't worry, its in the gitignore).")
+    exit()
+site.login(usr, pas)
 
 page = site.pages["New page name"]
 page.edit("Hello World! (Relma wuz here)")
