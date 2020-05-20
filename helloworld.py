65385a3f3aff9d7b2479c06ccb4347d8cc793449
Amulya Mohan <ammohan@microsoft.com>
2020-05-20 15:38:50 -0700

    allow anonymous editing and login. too lazy to figure out how 'sign in with Google' works

--- helloworld.py
+++ helloworld.py
@@ -8,7 +8,12 @@ site = mwclient.Site("myfirsttestwiki.fandom.com", path="/")
 if not (usr and (not usr.isspace())) or not (pas and (not pas.isspace())):
     warnings.warn("Put your fandom username and password in credentials.py (don't worry, its in the gitignore).")
     exit()
-site.login(usr, pas)
+try:
+    site.login(usr, pas)
+except:
+    #Edit anonymously instead
+    site.force_login = False
+
 
 page = site.pages["New page name"]
-page.edit("Hello World! (Relma wuz here)")
+page.edit("Hello World! (...)")
